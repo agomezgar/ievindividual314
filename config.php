89d7fdcfcbe671684e7728ec9869af479189e636
Antonio Gómez <agomezgar@gmail.com>
2015-09-24 13:28:30 +0200

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servidor = 'localhost';
-$base = 'ievindividual314';
-$usuario = 'root';
-$contra = 'apagonazo';
-$nombrecentro = 'IES Eduardo Valencia';$direccion = 'C/ Cervantes, Calzada de Calatrava (Ciudad Real)';
-$curso = '2011-2012';
-?>
